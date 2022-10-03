de7c52ce4e8fa81c8e7e4e09575343ccf85cd84e
Porndet Pipitsuntonsan <likeink2@hotmail.com>
2022-10-03 18:09:50 +0900

    new commit

--- Face-Detection.py
+++ Face-Detection.py
@@ -1,7 +1,7 @@
 import cv2
 
 face_cascade = cv2.CascadeClassifier('./data/haarcascades/haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('./data/haarcascades/haarcascade_eye.xml') 
+# eye_cascade = cv2.CascadeClassifier('./data/haarcascades/haarcascade_eye.xml') 
 
 cap = cv2.VideoCapture(0)
 
@@ -17,10 +17,10 @@ while 1:
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = img[y:y+h, x:x+w]
   
-        eyes = eye_cascade.detectMultiScale(roi_gray) 
+        # eyes = eye_cascade.detectMultiScale(roi_gray) 
   
-        for (ex,ey,ew,eh) in eyes:
-            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,127,255),2)
+        # for (ex,ey,ew,eh) in eyes:
+        #     cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,127,255),2)
   
     cv2.imshow('img',img)
 
