70f23575d7564666e01fd48aefb13082cbf725ef
Porndet Pipitsuntonsan <likeink2@hotmail.com>
2022-10-11 07:16:47 +0900

    new commit

--- Class/mysqlconnection.py
+++ Class/mysqlconnection.py
@@ -27,28 +27,3 @@ class MariaDB:
         self.mydb.commit()
         self.Mycursor.close()
         self.mydb.close()
-
-
-
-# mydb = mysql.connector.connect(
-#   host = "localhost",
-#   user = "root",
-#   password = "",
-#   database = "systemsecurity"
-# )
-
-# mycursor = mydb.cursor()
-
-# test1 = 1
-# test2 = 2
-# test3 = "2022-09-13 11:46:10"
-
-# data_user = (test1, test2, test3)
-
-# sql = "INSERT INTO record (speed, distance, date) values (%s, %s, %s)"
-# data_user = (float(test1), float(test2), test3)
-# mycursor.execute(sql, data_user)
-
-# mydb.commit()
-# mycursor.close()
-# mydb.close()
